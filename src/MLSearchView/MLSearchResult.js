29a0a56851996e35ece076742422019cd99d4569
Patrick McElwee <pmcelwee@gmail.com>
2017-10-03 16:52:29 -0400

    Pretty-print uri fallback
    By taking the 'filename', not the whole 'path'

--- src/MLSearchView/MLSearchResult.js
+++ src/MLSearchView/MLSearchResult.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Panel, Col } from 'react-bootstrap';
 import MLSearchSnippet from './MLSearchSnippet.js';
 
+const prettyUri = uri => {
+  const uriParts = uri.split('/');
+  return uriParts[uriParts.length - 1];
+};
+
 const MLSearchResult = ({result}) => (
   <Col xs={12} sm={6} md={4} lg={3}
     className="ml-search-result"
     key={result.uri}
   >
     <Panel>
-      <h4>{result.label || result.uri}</h4>
+      <h4>{result.label || prettyUri(result.uri)}</h4>
       <div className="ml-search-result-matches">
         {result.matches.map((match, index) =>
           <MLSearchSnippet match={match} key={index} />
