53a3e5a7af2e15b28779f915bdf4bc8ae9fa089d
Marianne Myers <Marianne.Myers@marklogic.com>
2017-10-27 16:48:00 -0600

    MSS-3 A detail page now shows a simple property

--- src/MLDetailView/MLDetailView.js
+++ src/MLDetailView/MLDetailView.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 class MLDetailView extends Component {
   constructor(props) {
     super(props);
+    this.loadDetail = this.loadDetail.bind(this);
+  }
+
+  componentWillMount() {
+    this.loadDetail(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.uri !== this.props.uri) {
+      this.loadDetail(nextProps);
+    }
+  }
+
+  loadDetail(props) {
+    const { uri } = props;
+    props.loadDetail(decodeURIComponent(uri));
   }
 
   render() {
     return (
       <div>
-        This is the detail!
+        {/* Since we're fetching data on page load, gotta check to make sure detail exists first */}
+        <b>eye color: </b>
+        {this.props.detail ? this.props.detail.eyeColor : '' }
       </div>
     );
   }
