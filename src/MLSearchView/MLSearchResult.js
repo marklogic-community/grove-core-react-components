709c6a79e1a2f276022cb08eb677b0bbade68946
Patrick McElwee <pmcelwee@gmail.com>
2017-11-10 11:55:06 -0500

    Make search result component injectable
    While still using <MLSearchResults>

--- src/MLSearchView/MLSearchResult.js
+++ src/MLSearchView/MLSearchResult.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Panel, Col } from 'react-bootstrap';
 import MLSearchSnippet from './MLSearchSnippet.js';
+import './MLSearchResult.css';
 
 const prettyUri = uri => {
   const uriParts = uri.split('/');
@@ -10,14 +11,15 @@ const prettyUri = uri => {
 const MLSearchResult = ({result}) => (
   <Col xs={12} sm={6} md={4} lg={3}
     className="ml-search-result"
-    key={result.uri}
   >
     <Panel>
       <h4>{result.label || prettyUri(result.uri)}</h4>
       <div className="ml-search-result-matches">
-        {result.matches.map((match, index) =>
-          <MLSearchSnippet match={match} key={index} />
-        )}
+        {
+          result.matches && result.matches.map((match, index) =>
+            <MLSearchSnippet match={match} key={index} />
+          )
+        }
       </div>
     </Panel>
   </Col>
