803a8b5d500edde83f9dd33156676c5e3d856220
Patrick McElwee <pmcelwee@gmail.com>
2018-01-10 14:18:00 -0500

    Add logo to navbar

--- src/MLNavbar/MLNavbar.js
+++ src/MLNavbar/MLNavbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Grid, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import logo from '../images/MarkLogic-Powered-By.png';
 
 const MLNavbar = (props) => {
   return (
-    <Navbar>
-      <Grid>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/">{props.title}</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-      </Grid>
+    <Navbar fluid={true}>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/" className="navbar-left">
+            <img src={props.logo || logo} />
+          </a>
+        </Navbar.Brand>
+        <Navbar.Brand>
+          <a href="/">{props.title}</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
     </Navbar>
   );
 };
