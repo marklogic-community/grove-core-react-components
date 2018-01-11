ed22fb92ffb9529883bd11dd753d0a9efd70d402
Patrick McElwee <pmcelwee@gmail.com>
2018-01-11 15:43:47 -0500

    Export all components
    I wonder if there is a better way to keep these in sync (ie, not manual)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+export MLFacets from './MLSearchView/MLFacets';
+export MLSearchMetrics from './MLSearchView/MLSearchMetrics';
+export MLSearchResponseView from './MLSearchView/MLSearchResponseView';
 export MLSearchBar from './MLSearchView/MLSearchBar';
 export MLSearchResults from './MLSearchView/MLSearchResults';
+export MLSearchResult from './MLSearchView/MLSearchResult';
 export MLSearchSnippet from './MLSearchView/MLSearchSnippet';
 export MLSearchView from './MLSearchView/MLSearchView';
 
 export MLNavbar from './MLNavbar/MLNavbar';
 
 export MLDetailView from './MLDetailView/MLDetailView';
+
+export FadeAndSlide from './animations/FadeAndSlide';
+export Fade from './animations/Fade';
