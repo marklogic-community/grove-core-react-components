cbd7594fef49fd306994b8f0a21bd87c32b6042e
Patrick McElwee <pmcelwee@gmail.com>
2018-01-18 11:20:11 -0500

    Allow Navbar content to be injected

--- src/MLNavbar/MLNavbar.js
+++ src/MLNavbar/MLNavbar.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import logo from '../images/MarkLogic-Powered-By.png';
 
-const MLNavbar = (props) => {
-  return (
-    <Navbar fluid={true}>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/" className="navbar-left">
-            <img src={props.logo || logo} />
-          </a>
-        </Navbar.Brand>
-        <Navbar.Brand>
-          <a href="/">{props.title}</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-    </Navbar>
-  );
-};
+const MLNavbar = (props) => (
+  <Navbar fluid={true}>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/" className="navbar-left">
+          <img src={props.logo || logo} />
+        </a>
+      </Navbar.Brand>
+      <Navbar.Brand>
+        <a href="/">{props.title}</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      {props.content}
+    </Navbar.Header>
+  </Navbar>
+);
 
 export default MLNavbar;
