099fbdccfaa3976ef099b9fa479c56ebb3a96cee
Patrick McElwee <pmcelwee@gmail.com>
2018-01-26 15:25:34 -0500

    <MLSearchResults> shows when there are no results
    There is a default message, which can be overridden by passing in a
    different component.
    
    Note that this means that consuming code is responsible for only showing
    MLSearchResults when a search is actually complete and results are still
    empty.

--- src/MLSearchView/MLSearchResults.js
+++ src/MLSearchView/MLSearchResults.js
@@ -3,22 +3,36 @@ import TransitionGroup from 'react-transition-group/TransitionGroup';
 import MLSearchResult from './MLSearchResult';
 import Fade from '../animations/Fade';
 
-const MLSearchResults = ({results, detailPath, resultComponent: ResultComponent}) => (
-  <div className="ml-search-results">
-    <TransitionGroup appear={true}>
-      {
-        results && results.map(result =>
-          <Fade duration={500} key={result.uri}>
-            <ResultComponent result={result}
-              detailPath={detailPath}
-            />
-          </Fade>
-        )
-      }
-    </TransitionGroup>
-  </div>
-);
+const DefaultNoResults = () => <p>No results matched your search.</p>;
 
-MLSearchResults.defaultProps = { resultComponent: MLSearchResult };
+const MLSearchResults = ({
+  results, detailPath,
+  noResults: NoResults, resultComponent: ResultComponent
+}) => {
+  if (!results) {return null;}
+  return (
+    <div className="ml-search-results">
+      <TransitionGroup appear={true}>
+        {
+          results.map(result =>
+            <Fade duration={500} key={result.uri}>
+              <ResultComponent result={result}
+                detailPath={detailPath}
+              />
+            </Fade>
+          )
+        }
+        {
+          (results.length === 0) && <Fade duration={500}><NoResults/></Fade>
+        }
+      </TransitionGroup>
+    </div>
+  );
+};
+
+MLSearchResults.defaultProps = {
+  resultComponent: MLSearchResult,
+  noResults: DefaultNoResults
+};
 
 export default MLSearchResults;
