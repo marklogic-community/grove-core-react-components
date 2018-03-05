0bc56d687842c35e88f517bbe1a4fc900b401588
Patrick McElwee <pmcelwee@gmail.com>
2018-03-05 11:57:54 -0500

    Ability to pick Card or List result type
    This is not likely to be the final API

---
+++ src/MLSearchView/ListResult.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import MLSearchSnippet from './MLSearchSnippet.js';
+
+const prettyUri = uri => {
+  const uriParts = uri.split('/');
+  return uriParts[uriParts.length - 1];
+};
+
+const ListResult = (props) => (
+  <Col xs={12}
+    className="ml-search-result"
+  >
+    <h4>
+      <Link to={props.detailPath + encodeURIComponent(props.result.uri)}>
+        {props.result.label || prettyUri(props.result.uri)}
+      </Link>
+    </h4>
+    <div className="ml-search-result-matches">
+      {
+        props.result.matches && props.result.matches.map((match, index) =>
+          <MLSearchSnippet match={match} key={index} />
+        )
+      }
+    </div>
+    <hr style={{borderTopColor: 'white'}}/>
+  </Col>
+);
+
+export default ListResult;
--- src/MLSearchView/MLSearchResponseView.js
+++ src/MLSearchView/MLSearchResponseView.js
@@ -1,53 +1,107 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Pagination } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Pagination,
+  ButtonToolbar,
+  ToggleButtonGroup,
+  ToggleButton
+} from 'react-bootstrap';
 
 import MLSearchResults from './MLSearchResults';
 import MLSearchMetrics from './MLSearchMetrics';
+import CardsResult from './MLSearchResult';
+import ListResult from './ListResult';
 
-const MLSearchResponseView = (props) => (
-  // TODO: better error rendering
-  <div>
-    {props.error &&
-      <div>
-        <p>
-          There was an error performing your search. The server sent the following error message:
-        </p>
-        <p className="text-danger">{props.error}</p>
-      </div>
+class MLSearchResponseView extends Component {
+  constructor(props) {
+    super(props);
+    const resultComponentName = props.resultComponent ? '' : 'Cards';
+    this.state = {
+      resultComponentName: resultComponentName,
+      resultComponent: props.resultComponent
+    };
+    this.setResultType = this.setResultType.bind(this);
+  }
+
+  setResultType(e) {
+    let resultComponent;
+    if (e === 'Cards') {
+      resultComponent = CardsResult;
+    } else if (e === 'List') {
+      resultComponent = ListResult;
+    } else {
+      throw 'Invalid Result Type: ' + e;
     }
-    {!props.error &&
+    this.setState({
+      resultComponentName: e,
+      resultComponent: resultComponent
+    });
+  }
+
+  render() {
+    // TODO: better error rendering
+    return (
       <div>
-        <Row>
-          <Col md={12}>
-            <MLSearchMetrics
-              time={props.executionTime}
-              total={props.total}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <MLSearchResults
-            className="ml-search-results"
-            results={props.results || []}
-            detailPath={props.detailPath}
-            resultComponent={props.resultComponent}
-          />
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Pagination
-              items={props.totalPages}
-              maxButtons={10}
-              boundaryLinks={true}
-              activePage={props.page}
-              onSelect={props.handlePageSelection} />
-          </Col>
-        </Row>
+        {this.props.error &&
+          <div>
+            <p>
+              There was an error performing your search.
+              The server sent the following error message:
+            </p>
+            <p className="text-danger">{this.props.error}</p>
+          </div>
+        }
+        {!this.props.error &&
+          <div>
+            <Row>
+              <Col md={12}>
+                <MLSearchMetrics
+                  time={this.props.executionTime}
+                  total={this.props.total}
+                />
+                <ButtonToolbar style={{float: 'right'}}>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="result-options"
+                    value={this.state.resultComponentName}
+                    onChange={this.setResultType}
+                  >
+                    <ToggleButton value="Cards">
+                      Cards
+                    </ToggleButton>
+                    <ToggleButton value="List">
+                      List
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
+              </Col>
+            </Row>
+            <Row>
+              <MLSearchResults
+                className="ml-search-results"
+                results={this.props.results || []}
+                detailPath={this.props.detailPath}
+                resultComponent={this.state.resultComponent}
+              />
+            </Row>
+            <Row>
+              <Col md={12}>
+                <Pagination
+                  items={this.props.totalPages}
+                  maxButtons={10}
+                  boundaryLinks={true}
+                  activePage={this.props.page}
+                  onSelect={this.props.handlePageSelection} />
+              </Col>
+            </Row>
+          </div>
+        }
       </div>
-    }
-  </div>
-);
+    );
+  }
+}
 
 MLSearchResponseView.propTypes = {
   error: PropTypes.string,
