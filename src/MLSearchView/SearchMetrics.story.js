3d1b31963a265abf4b6a601469e26c56f7b04118
Patrick McElwee <pmcelwee@gmail.com>
2018-03-08 12:29:39 -0500

    Storie for <SearchMetrics />

---
+++ src/MLSearchView/SearchMetrics.story.js
@@ -0,0 +1,11 @@
+/* global module */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import SearchMetrics from './SearchMetrics';
+
+storiesOf('SearchResponseView/SearchMetrics', module)
+  .add('default', () => <SearchMetrics time={0.090908989} total={1000} />)
+  .add('without time', () => <SearchMetrics total={1000} />)
+  .add('without total', () => <SearchMetrics time={0.090908989} />)
+  .add('with nothing', () => <SearchMetrics />);
