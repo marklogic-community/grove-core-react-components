b781519a50133c78ae3bd5a23303a5c240da3a91
Patrick McElwee <pmcelwee@gmail.com>
2018-07-18 15:46:53 -0400

    Correct syntax for re-exports

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-export { SearchView } from './SearchView/SearchView';
+export { default as SearchView } from './SearchView/SearchView';
 
-export { SearchBar } from './SearchView/SearchBar';
+export { default as SearchBar } from './SearchView/SearchBar';
 
-export { Facets } from './SearchView/Facets/Facets';
-export { CurrentFilters } from './SearchView/Facets/CurrentFilters';
-export { SingleFilterList } from './SearchView/Facets/SingleFilterList';
+export { default as Facets } from './SearchView/Facets/Facets';
+export { default as CurrentFilters } from './SearchView/Facets/CurrentFilters';
+export {
+  default as SingleFilterList
+} from './SearchView/Facets/SingleFilterList';
 
-export { SearchResponseView } from './SearchView/SearchResponseView';
-export { SearchMetrics } from './SearchView/SearchMetrics';
+export { default as SearchResponseView } from './SearchView/SearchResponseView';
+export { default as SearchMetrics } from './SearchView/SearchMetrics';
 
-export { SearchResults } from './SearchView/SearchResults/SearchResults';
-export { CardResult } from './SearchView/SearchResults/CardResult';
-export { ListResult } from './SearchView/SearchResults/ListResult';
-export { SearchSnippet } from './SearchView/SearchResults/SearchSnippet';
+export {
+  default as SearchResults
+} from './SearchView/SearchResults/SearchResults';
+export { default as CardResult } from './SearchView/SearchResults/CardResult';
+export { default as ListResult } from './SearchView/SearchResults/ListResult';
+export {
+  default as SearchSnippet
+} from './SearchView/SearchResults/SearchSnippet';
 
-export { Navbar } from './Navbar/Navbar';
+export { default as Navbar } from './Navbar/Navbar';
 
-export { Login } from './user/Login';
-export { LoginJumbotron } from './user/LoginJumbotron';
+export { default as Login } from './user/Login';
+export { default as LoginJumbotron } from './user/LoginJumbotron';
 
-export { DetailView } from './DetailView/DetailView';
+export { default as DetailView } from './DetailView/DetailView';
 
-export { FadeAndSlide } from './animations/FadeAndSlide';
-export { Fade } from './animations/Fade';
+export { default as FadeAndSlide } from './animations/FadeAndSlide';
+export { default as Fade } from './animations/Fade';
