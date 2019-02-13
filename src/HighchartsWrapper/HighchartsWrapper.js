40361ba6965ed784ed5a7f98db1d70a4fa02a3af
Nick LaPrell <Nick.LaPrell@marklogic.com>
2019-02-13 09:16:03 -0700

    Adding documentation.

--- src/HighchartsWrapper/HighchartsWrapper.js
+++ src/HighchartsWrapper/HighchartsWrapper.js
@@ -1,3 +1,12 @@
+/* HighchartsWrapper.js
+ *
+ * This component is a wrapper around the Highcharts library with a default configuration.
+ *
+ * PROPS
+ * - config  - a path to an external config file represented as a string, or an object representing Highcharts options (see: https://api.highcharts.com/highcharts/)
+ * - modules - An array of Highcharts modules to include in the chart. Each array value should be an imported Highcharts module.
+ * - load    - A function to be called once the chart is rendered on the screen. Typically used to load external data and update the chart.
+ */
 import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
