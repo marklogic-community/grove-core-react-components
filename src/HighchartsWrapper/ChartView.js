78747950981861476b6a43eb311c49188edafd03
Nick LaPrell <Nick.LaPrell@marklogic.com>
2019-02-13 10:57:21 -0700

    Refactored the ChartView component to use props instead of class inheritance.

--- src/HighchartsWrapper/ChartView.js
+++ src/HighchartsWrapper/ChartView.js
@@ -1,96 +1,89 @@
 /* ChartView.js
  *
- * This component is meant to be used as an interface. The following methods and state variables can be implemented:
+ * This component is a more advanced wrapper for Highcharts than the HighchartsWrapper component.
+ * It includes a default data loading feature and handlers for initial chart load, failed data load (when using the default fetch),
+ * and successful data loaded (when using the default fetch).
  *
- * loadData() - Optional. Overrides the default data loading mechanism. The function gets called when rendering the chart.
+ * The event flow is construct -> load -> fetch -> dataLoaded/loadFailed
  *
- * dataLoaded() - Called after data is successfully loaded by the default loadData() function.
- * If loadData() is overloaded, it will not get called unless explicitly called from the overloading
- * function.
+ * The following properties define the chart:
+ * - config     - a path to an external config file represented as a string, or an object representing Highcharts options (see: https://api.highcharts.com/highcharts/)
+ * - modules    - An array of Highcharts modules to include in the chart. Each array value should be an imported Highcharts module.
+ * - load       - A function to be called once the chart is rendered on the screen. Typically used to load external data and update the chart. A reference to the component is
+ *                sent as the first argument.
+ * - fetch      - An object representing how the default fetch mechanism should be used to fetch data for the chart. Fetch can have the following properties:
+ *                * url    - URL from which to fetch the data (required).
+ *                * method - HTTP method used for the fetch. (default: GET)
+ *                * type   - Content header (default: application/json)
+ *                * body   - Body to send (default: undefined)
+ * - loadFailed - A function to be called if the default fetch failed. A reference to the component is sent as the first argument.
+ * - dataLoaded - A function to be called when the default fetch successfully returns data. A reference to the component is sent as the first argument.
  *
- * loadFailed() - Called by the default loadData() function when the call results in an error response. this.state.lastResponse should be examined
- * for status and statusText values. If loadData() is overloaded, it will not get called unless explicitly called from the overloading
- * function.
- *
- * this.state.fetch - Defines parameters used by the default loadData() function.
- * Example:
- *   fetch: {
- *     type: 'GET',
- *     url: '/api/my-end-point',
- *     body: JSON.stringify({message:'hi'})
- *   }
- *
- * this.state.config - A configuration to pass into Highcharts. This can be a JSON object, or a string reference to a file in /components/highcharts-config
- *
- * this.state.lastResponse is an object where:
- *   status - http response code of the last call.
- *   ok - true/false indicating last response success.
- *   data - Data returned by the last call.
- *
- * Raw data as it is retrieved from the default loadData() function is stored in this.state.data.
- * The Highcharts class instance is stored in this.state.Highcharts. It can be used to make calls to the library functions, or reference charts (this.state.Highcharts.charts[0]... etc).
- *
- * A Basic implementation will set fetch parameters in state.fetch, define a chart config in this.state.config, and impliment a loadData(response) function to process the
- * endpoint response and manipulate this.state.Highcharts and/or this.state.Highcharts.charts[0].
+ * Handlers are sent a reference to the component for context when called:
+ * - state.Highcharts is a reference to the chart object.
+ * - state.lastResponse contains results from the default fetch:
+ *   * status - Status response
+ *   * ok     - Boolean value representing response success.
+ *   * data   - The data returned.
  */
 import React, { Component } from 'react';
-import _ from 'lodash';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import Options from './highchartsDefaultOptions.js';
 
 class ChartView extends Component {
-  // Extend the default highchart options with the string or object stored in this.state.config.
-  setConfig() {
-    var config = this.state.config || {};
+  constructor(props) {
+    super(props);
+    // Create a Highcharts options object
     var opt = new Options(Highcharts);
-    opt.extend({ config: config });
+    // Extend Highcharts options with supplied config/modules props
+    opt.extend({
+      config: props.config || {},
+      modules: props.modules || []
+    });
+    // Lang options have to be defined up-front.
     opt.Highcharts.setOptions({
       lang: {
         thousandsSep: ','
       }
     });
-    this.setState({
+    this.state = {
+      // Initial config
       opt: opt.values,
+      // Highcharts object reference
       Highcharts: opt.Highcharts,
+      // Optional fetch parameters for default fetch function
+      fetch: props.fetch || false,
+      // Optional load called when mounting the component
+      load: props.load || false,
+      // Optional fail function when default fetch fails
+      loadFailed: props.loadFailed || false,
+      // Optional success function when default fetch is successful
+      dataLoaded: props.dataLoaded || false,
+      // Stores the last response data from the fetch function
       lastResponse: {
         status: null,
         data: null,
         ok: null
       }
-    });
+    };
   }
 
-  // Call setConfig() after instantiation and before rendering.
-  componentWillMount() {
-    this.setConfig();
-  }
-
-  // Call loadData() once the initial chart is rendered.
   componentDidMount() {
     this.loadData();
   }
 
-  // Call loadData() if the context of this component changes.
-  componentDidUpdate(previousProps) {
-    if (!_.isEqual(previousProps, this.props)) {
-      this.loadData();
-    }
-  }
-
   // Default data loading and handling function.
-  // Note: This can be overloaded with the implimenting class.
-  // Calls fetch with state.fetch parameters, then calls dataLoaded(response).
-  // Note: dataLoaded must be explicitly implimented or it will not be called.
-  loadData() {
+  // Calls fetch with state.fetch parameters, then calls the optional dataLoaded() or loadFailed() function.
+  fetchData() {
     // Only execute if fetch parameters have been set
     if (!this.state.fetch || !this.state.fetch.url) {
       return false;
     }
     let params = {
-      method: this.state.fetch.type || 'GET',
+      method: this.state.fetch.method || 'GET',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': this.state.fetch.type || 'application/json'
       }
     };
     if (this.state.fetch.body) {
@@ -104,8 +97,11 @@ class ChartView extends Component {
           ok: resp.status >= 200 && resp.status < 300
         };
         this.setState({ lastResponse: responseState });
-        if (this.state.lastResponse.ok) {
-          return resp.json();
+        if (resp.ok) {
+          return this.state.fetch.type === 'application/json' ||
+            !this.state.fetch.type
+            ? resp.json()
+            : resp;
         } else {
           this.loadFailed();
         }
@@ -117,21 +113,31 @@ class ChartView extends Component {
             state.lastResponse.data = data;
             return state;
           });
-          if (typeof this.dataLoaded === 'function') {
-            this.dataLoaded();
-          }
+          this.dataLoaded();
         }
       });
   }
 
-  // Called after default loadData() function returns successfully.
-  dataLoaded() {
-    // Implement me
+  // Called on component mount
+  loadData() {
+    if (this.state.load) {
+      this.state.load(this);
+    }
+    this.fetchData();
   }
 
-  // Called when default loadData() function fails.
+  // Called after default fetchData() function returns successfully.
+  dataLoaded() {
+    if (this.state.dataLoaded) {
+      this.state.dataLoaded(this);
+    }
+  }
+
+  // Called when default fetchData() function fails.
   loadFailed() {
-    // Implement me
+    if (this.state.loadFailed) {
+      this.state.loadFailed(this);
+    }
   }
 
   render() {
